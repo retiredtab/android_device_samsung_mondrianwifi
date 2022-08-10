54b7e7c527bb318158e487e765469c134b969c17
Kyle Harrison <khwebmail@gmail.com>
2022-08-10 17:53:45 -0600

    mondrianwifi: Move ueventd.qcom.rc to /vendor/etc
    In Android 12, the search path for the ueventd vendor config is now /vendor/etc.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -27,7 +27,7 @@ LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
