56106b383f90961420adb0ceb5bebcd56e292883
Edwin Wong <edwinwong@google.com>
2022-08-10 22:38:10 -0600

    mondrianwifi: Update clearkey plugin to drm HAL v1.4
    Must update to 1.4 because 1.3 fails to work in Android 12.

--- device.mk
+++ device.mk
@@ -56,7 +56,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # FlipFlap
 PRODUCT_PACKAGES += \
